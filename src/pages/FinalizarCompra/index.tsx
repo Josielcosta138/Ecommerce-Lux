0c9b3a56db455bb4c4a672248a34a52fe2201375
Josielcosta138 <63210548+Josielcosta138@users.noreply.github.com>
2024-07-01 20:02:10 -0300

    teste final

--- src/pages/FinalizarCompra/index.tsx
+++ src/pages/FinalizarCompra/index.tsx
@@ -1,40 +1,55 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { carregarCarrinho } from "../../store/CarrinhoStore/carrinhoStore";
+import { Button, InputLabel, TextField, Modal, Box, Typography } from "@mui/material";
 import "./index.css";
+import "../../pages/Clientes/index.css";
+import { wait } from "@testing-library/user-event/dist/utils";
 
 const FinalizarCompra: FC = () => {
   const navigate = useNavigate();
   const carrinho = carregarCarrinho();
+  const [open, setOpen] = useState(false);
+  const [modalMessage, setModalMessage] = useState<string>("");
+  const handleClose = () => setOpen(false);
 
-  const handleFinalizarCompra = () => {
+  const finalizarCompra = () => {
 
     const clienteJSON = localStorage.getItem("cliente");
-    if (clienteJSON) {
-      const cliente = JSON.parse(clienteJSON);
-      const nomeCliente = cliente.nome || "";
-      const emailCliente = cliente.email || "";
-      const idCliente = cliente.id || "";
+        
+      if (clienteJSON) {
+          const cliente = JSON.parse(clienteJSON);
+          const nomeCliente = cliente.nome || "";
+          const emailCliente = cliente.email || "";
+          const idCliente = cliente.id || "";
 
 
-      // Armazena os dados no localStorage para a página de checkout
-      localStorage.setItem("nomeCliente", nomeCliente);
-      localStorage.setItem("emailCliente", emailCliente);
-      localStorage.setItem("idCliente", idCliente);
+          // Armazena os dados no localStorage para a página de checkout
+          localStorage.setItem("nomeCliente", nomeCliente);
+          localStorage.setItem("emailCliente", emailCliente);
+          localStorage.setItem("idCliente", idCliente);
 
-      navigate("/checkout");
-    } else {
-      alert("Não há informações de cliente no localStorage.");
-    }
+          navigate("/checkout");
+      } else {
+          setOpen(true);
+          setModalMessage("Não há cliente logado ao sistema!")
+
+            setTimeout(() => {
+              setOpen(false);
+            } ,5000);
+        }
   };
 
-  const handleContinuarComprando = () => {
+
+  const continuarComprando = () => {
     navigate("/produtos");
   };
+
 
   const calcularTotal = () => {
     return carrinho.reduce((total, item) => total + item.preco * item.quantidade, 0).toFixed(2);
   };
+
 
   return (
     <div className="finalizar-compra-container">
@@ -55,16 +70,33 @@ const FinalizarCompra: FC = () => {
           <p>Seu carrinho está vazio.</p>
         )}
       </div>
+      <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box className="modal-box">
+                    <Typography id="modal-modal-title" variant="h6" component="h2">
+                        Algo deu errado :/
+                    </Typography>
+                    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                        {modalMessage}
+                    </Typography>
+                </Box>
+            </Modal>
+
+
       {carrinho.length > 0 && (
         <div className="resumo-compra">
           <h2>Resumo do Pedido</h2>
           <div className="total-compra">
             <span>Total: R$ {calcularTotal()}</span>
           </div>
-          <button onClick={handleFinalizarCompra} className="btn-finalizar-compra">
+          <button onClick={finalizarCompra} className="btn-finalizar-compra">
             Finalizar Compra
           </button>
-          <button onClick={handleContinuarComprando} className="btn-continuar-comprando">
+          <button onClick={continuarComprando} className="btn-continuar-comprando">
             Continuar Comprando
           </button>
         </div>
